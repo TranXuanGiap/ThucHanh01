5798839865b0f3c081c0ef0346aae52ff791b217
giap <giap2k51111@gmail.com>
2025-11-15 00:16:27 +0700

    Add Lab3-4 project into solution folder

---
+++ Lab3-4/Data/MyWebAppContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Lab3_4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab3_4.Data;
+
+public partial class MyWebAppContext : DbContext
+{
+    public MyWebAppContext()
+    {
+    }
+
+    public MyWebAppContext(DbContextOptions<MyWebAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Learner> Learners { get; set; }
+
+    public virtual DbSet<Major> Majors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-LQAGRB3F;Database=MyWebAppDB;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PK__Course__C92D7187DE7121FF");
+
+            entity.ToTable("Course");
+
+            entity.Property(e => e.CourseId)
+                .ValueGeneratedNever()
+                .HasColumnName("CourseID");
+            entity.Property(e => e.Title).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.EnrollmentId).HasName("PK__Enrollme__7F6877FB475C5323");
+
+            entity.ToTable("Enrollment");
+
+            entity.Property(e => e.EnrollmentId).HasColumnName("EnrollmentID");
+            entity.Property(e => e.CourseId).HasColumnName("CourseID");
+            entity.Property(e => e.LearnerId).HasColumnName("LearnerID");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.CourseId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__Cours__3E52440B");
+
+            entity.HasOne(d => d.Learner).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.LearnerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__Learn__3F466844");
+        });
+
+        modelBuilder.Entity<Learner>(entity =>
+        {
+            entity.HasKey(e => e.LearnerId).HasName("PK__Learner__67ABFCFA7B305D17");
+
+            entity.ToTable("Learner");
+
+            entity.Property(e => e.LearnerId).HasColumnName("LearnerID");
+            entity.Property(e => e.FirstMidName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.MajorId).HasColumnName("MajorID");
+
+            entity.HasOne(d => d.Major).WithMany(p => p.Learners)
+                .HasForeignKey(d => d.MajorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Learner__MajorID__398D8EEE");
+        });
+
+        modelBuilder.Entity<Major>(entity =>
+        {
+            entity.HasKey(e => e.MajorId).HasName("PK__Major__D5B8BFB112F80575");
+
+            entity.ToTable("Major");
+
+            entity.Property(e => e.MajorId).HasColumnName("MajorID");
+            entity.Property(e => e.MajorName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Lab3-4/Program.cs
@@ -0,0 +1,28 @@
+using Lab3_4.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
